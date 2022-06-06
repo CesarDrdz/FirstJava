29408536b9d5727a9ea41f395bee1e67b7ca2487
John Doe <johndoe@example.com>
2022-06-06 15:44:59 -0800

    initial commit

---
+++ FirstJava.java
@@ -0,0 +1,9 @@
+public class FirstJava {
+	public static void main(String[] args) {
+
+    System.out.println("My name is Cesar");
+    System.out.println("I am 29 y/o");
+    System.out.println("My hometown is Fresno, CA");
+
+	}
+}
